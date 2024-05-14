d73c68d063d3748763a590f5521108ae62426322
BuildTools <unconfigured@null.spigotmc.org>
2024-05-14 19:46:16 -0300

    Update: Implementando validações, e method getAllRooms

--- src/main/java/com/hotelcheckin/hotel/controller/CheckInController.java
+++ src/main/java/com/hotelcheckin/hotel/controller/CheckInController.java
@@ -1,12 +1,18 @@
 package com.hotelcheckin.hotel.controller;
 
 
+import com.hotelcheckin.hotel.exception.InvalidDateException;
+import com.hotelcheckin.hotel.exception.InvalidRoomTypeException;
 import com.hotelcheckin.hotel.model.Reservation;
 import com.hotelcheckin.hotel.model.ReservationRequest;
+import com.hotelcheckin.hotel.model.Room;
 import com.hotelcheckin.hotel.service.CheckInService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -18,14 +24,24 @@ public class CheckInController {
     }
 
     @PostMapping("/reserve")
-    public ResponseEntity<Object> makeReservation(@RequestBody ReservationRequest request) {
-        
+    public ResponseEntity<Object> makeReservation(@Valid @RequestBody ReservationRequest request) {
+
         try {
             Reservation reservation = checkInService.makeReservation(request);
             return ResponseEntity.ok(reservation);
-        } catch (RuntimeException e) {
+        }
+        catch (RuntimeException e) {
+            if(e.getMessage().contains("No enum constant")) {
+                throw new InvalidRoomTypeException("Tipo de quarto inválido.");
+            }
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
+    }
+
+    @GetMapping("/rooms")
+    public ResponseEntity<List<Room>> getRooms() {
+        List<Room> rooms = checkInService.getAllRooms();
+        return ResponseEntity.ok(rooms);
     }
 
     @GetMapping("/hello")
